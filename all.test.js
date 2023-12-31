bc36c9aa7c1a3c52f32d3dc8286532b5c9b15476
Rutabadev <etienner37@gmail.com>
2023-12-31 15:50:08 +0100

    homogenize undescores in test numbers

--- all.test.js
+++ all.test.js
@@ -6,34 +6,34 @@ import * as day4 from "./4";
 
 describe("day 1", () => {
   test("question 1", () => {
-    expect(day1.question1()).toEqual(54081);
+    expect(day1.question1()).toEqual(54_081);
   });
   test("question 2", () => {
-    expect(day1.question2()).toEqual(54649);
+    expect(day1.question2()).toEqual(54_649);
   });
 });
 
 describe("day 2", () => {
   test("question 1", () => {
-    expect(day2.question1()).toEqual(2716);
+    expect(day2.question1()).toEqual(2_716);
   });
   test("question 2", () => {
-    expect(day2.question2()).toEqual(72227);
+    expect(day2.question2()).toEqual(72_227);
   });
 });
 
 describe("day 3", () => {
   test("question 1", () => {
-    expect(day3.question1()).toEqual(550064);
+    expect(day3.question1()).toEqual(550_064);
   });
   test("question 2", () => {
-    expect(day3.question2()).toEqual(85010461);
+    expect(day3.question2()).toEqual(85_010_461);
   });
 });
 
 describe("day 4", () => {
   test("question 1", () => {
-    expect(day4.question1()).toEqual(28750);
+    expect(day4.question1()).toEqual(28_750);
   });
   test.skip("question 2 with caching", () => {
     expect(day4.question2()).toEqual(10_212_704);
