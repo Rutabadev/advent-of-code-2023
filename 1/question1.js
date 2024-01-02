52c59e71e9a8e2ad8c42e2ce2d8eba4feb33bfa8
Rutabadev <etienner37@gmail.com>
2024-01-02 23:06:51 +0100

    added regex explaination for day 1 question 1

--- 1/question1.js
+++ 1/question1.js
@@ -2,7 +2,7 @@ import input from "./input.js";
 
 export function computeAnswer() {
   return input.split("\n").reduce((acc, line) => {
-    const firstDigit = line.match(/^.*?(\d)/)?.[1];
+    const firstDigit = line.match(/^.*?(\d)/)?.[1]; // putting ? at the end of ".*?" makes it non greedy and match as little as possible
     const lastDigit = line.match(/^.*(\d)/)?.[1];
     if (!firstDigit || !lastDigit)
       throw new Error(`No first or last digit found in ${line}`);
